d0d17da1605925be75a5657b7eadf6288aed6808
imakero <45946863+imakero@users.noreply.github.com>
2022-04-20 11:02:02 +0200

    feature: login

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Box, ChakraProvider } from "@chakra-ui/react"
+import { UserProvider } from "../context/UserContext"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <ChakraProvider>
+        <Box w="100%" align="center">
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </UserProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/login.js
@@ -0,0 +1,94 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import { useState, useContext, useEffect } from "react"
+import InternalLink from "../components/InternalLink"
+import { UserContext } from "../context/UserContext"
+import { setAuthToken } from "../lib/auth"
+
+const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+  const { user, setUser } = useContext(UserContext)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user) {
+      router.push("/")
+    }
+  }, [router, user])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const login = async (username, password) => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      })
+      const data = await res.json()
+      if (data.error) {
+        setError(data.error)
+      } else {
+        setAuthToken(data.token)
+        setUser(data)
+        router.push("/")
+      }
+    }
+    login(username, password)
+  }
+
+  // If already logged in, don't render anything and wait for redirect
+  if (user) {
+    return null
+  }
+
+  return (
+    <VStack maxW="300px" w="300px" align="start">
+      <Heading>Log in</Heading>
+      <Text>
+        Don&apos;t have an account?{" "}
+        <InternalLink href="/signup" color="teal">
+          Sign up
+        </InternalLink>
+      </Text>
+      <form onSubmit={handleSubmit}>
+        <Box align="start">
+          <FormControl>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              id="username"
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </FormControl>
+          {error && <Text color="red.400">{error.message}</Text>}
+          <Button type="submit">Log in</Button>
+        </Box>
+      </form>
+    </VStack>
+  )
+}
+
+export default Login
