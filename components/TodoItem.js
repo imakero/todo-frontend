128cab9194f4dfc9454a21eac9036894632e1197
imakero <45946863+imakero@users.noreply.github.com>
2022-04-22 09:14:07 +0200

    feature: toggle todos

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,9 +1,24 @@
 import { Checkbox, HStack } from "@chakra-ui/react"
+import { useMutation, useQueryClient } from "react-query"
+import { toggleTodo } from "../lib/apiWrappers"
 
 const TodoItem = ({ todo }) => {
+  const queryClient = useQueryClient()
+  const toggleTodoMutation = useMutation(toggleTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos")
+    },
+  })
+
+  const handleChange = () => {
+    toggleTodoMutation.mutate({ todoId: todo._id, completed: todo.completed })
+  }
+
   return (
     <HStack>
-      <Checkbox isChecked={todo.completed}>{todo.title}</Checkbox>
+      <Checkbox isChecked={todo.completed} onChange={handleChange}>
+        {todo.title}
+      </Checkbox>
     </HStack>
   )
 }
